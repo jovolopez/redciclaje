72426aec9654f6618eaa2414de008d32a143c1db
Javveru <thezepols@gmail.com>
2020-04-10 22:23:06 -0300

    Master layout desktop: 100% & Home preview

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,13 @@
 @extends('layout/master')
+@section('css')
+    <link rel="stylesheet" href="/css/home.css">
+@endsection
+@section('content')
+    <div class="banner-one">
+    </div>
+    <div class="main-text-cont">
+        <h1>REDCICLAJE</h1>
+        <p>Redciclaje nace como proyecto a modo de acción ante la urgencia ambiental actual, en donde no sólo es necesaria la intención de reciclar sino hacerlo de manera consciente y adecuada, teniendo en cuenta que el Reciclaje no es sino el último eslabón en los pasos para una economía circular (reducir, reutilizar, reciclar). </p>
+        <p>De esta manera, se busca informar y facilitar la coordinación a una comunidad de usuarios interesados para reciclar de forma adecuada, además de invitar cada vez a más personas a unirse al cambio.</p>
+    </div>
+@endsection
